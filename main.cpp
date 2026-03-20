c3cb27d737e08ceabae0a636f50b4cb03ff615e1
Damian Vega Meza <damian@LAPTOP-H44S0OTB.localdomain>
2026-03-19 18:25:06 -0700

    Assignment done

--- main.cpp
+++ main.cpp
@@ -12,7 +12,20 @@ int main()
 
     // TODO
     // Find max, min, mid using only if-statements (no loops, no functions, no arrays)
+      minVal = num1;
+    if(num2 < minVal) {
+        minVal = num2;
+    } if (num3 < minVal) {
+        minVal = num3;
+    }
+    maxVal = num1;
+    if(num2 > maxVal) {
+        maxVal = num2;
+    } if (num3 > maxVal) {
+        maxVal = num3;
+    }
 
+    midVal = (num1 + num2 +  num3) - maxVal -minVal;
     cout << maxVal << " " << midVal << " " << minVal << endl;
 
     return 0;
